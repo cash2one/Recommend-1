ff6c6d85dc2190577aad03cd429c59f10cbf18c4
zhouzebiao <447523426@qq.com>
2016-08-03 21:00:42 +0800

    uic

--- sql/tmp.sql
+++ sql/tmp.sql
@@ -1,9 +1,1 @@
 use recommend;
-drop table item_usernum;
-create table tb_item_usernum
-SELECT item_id, count(user_id)as alluser,count(distinct user_id )as actuser
-FROM tb_tianchi_user
-GROUP BY item_id,user_id
-ORDER BY actuser DESC,alluser DESC;
-
-select * from tb_item_usernum;
