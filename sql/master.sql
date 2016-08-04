db5a286a5a2d1954e264aa1331e0f9f82a787389
zhouzebiao <447523426@qq.com>
2016-08-04 21:00:21 +0800

    master

--- sql/master.sql
+++ sql/master.sql
@@ -1,15 +1,2 @@
 use recommend;
- -- all predict
-select count(a.user_id) from (
-select user_id,item_id from tb_test_predict 
-where time >='2014-12-14'
-group by user_id,item_id 
-order by user_id
-)as a
-;
-
--- 18 buy 
-create table tb_test_result as
-select * from tb_tianchi_user where
-(time>='2014-12-18 00' and time<='2014-12-18 24' and behavior_type ='4')
 
--- sql/test_and_real_dataset.sql
+++ sql/test_and_real_dataset.sql
@@ -15,3 +15,28 @@ or
 (time>='2014-12-18 00' and time<='2014-12-18 24');
 
 select * from tb_real_predict;
+
+
+ -- all predict
+select count(a.user_id) from (
+select user_id,item_id from tb_test_predict 
+where time >='2014-12-14'
+group by user_id,item_id 
+order by user_id
+)as a
+;
+
+-- 18 buy 
+create table tb_test_result as
+select * from tb_tianchi_user where
+(time>='2014-12-18 00' and time<='2014-12-18 24' and behavior_type ='4')
+;
+
+select user_id,item_id from tb_tianchi_user where 
+(time >='2014-12-18 00' and time <='2014-12-18 24')
+group by user_id,item_id
+order by user_id
+into outfile 'D:/asd.csv'
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'; 
