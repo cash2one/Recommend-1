3e4885397af314b47cfa9f3d0d9730c611bf6e6c
zhouzebiao <447523426@qq.com>
2016-08-10 16:20:39 +0800

    men

--- i_feature.py
+++ i_feature.py
@@ -10,8 +10,7 @@ import winsound
 import csv
 
 
-def i_train():
-    i_list = []
+def get_i_feature():
     f = file(Path.i_feature, 'r')
     reader = csv.reader(f)
     i_all = {}
@@ -19,8 +18,11 @@ def i_train():
         i_all[item_id] = {'all_all1': all1, 'all_all2': all2, 'all_all3': all3, 'all_all4': all4,
                           'all_allcount': allcount, 'all_act1': act1, 'all_act2': act2, 'all_act3': act3,
                           'all_act4': act4,
-                          'all_actcount': actcount}
-    print i_all
+                          'all_actcount': actcount, 'result': 0}
+    return i_all
+
+
+def get_result(i):
     db = MySQLdb.connect(
         host=Path.host,
         port=3306,
@@ -28,40 +30,38 @@ def i_train():
         passwd="1234",
         db="recommend"
     )
-    for i in range(shape(Path.tb_train)[0]):
-        cursor = db.cursor()
-        sql = "select item_id,all1,all2,all3,all4,allcount,act1,act2,act3,act4,actcount from " + Path.tb_i_feature[i]
+    cursor = db.cursor()
+    sql = "select DISTINCT item_id from " + Path.tb_train_result[i] + " where behavior_type='4'"
+    i7_result = {}
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            i7_result[row[0]] = 1
+    except:
+        print '\033[1;31;m'
+        print 'db.rollback()7result'
+        print '\033[0m'
+        db.rollback()
+    db.close()
+    return i7_result
+
+
+def i_train():
+    i_list = []
+    i_all = get_i_feature()
+    for i in range(shape(Path.tb_feature_i)[0]):
+        f = file(Path.tb_feature_i[i], 'r')
+        reader = csv.reader(f)
         i7 = {}
-        try:
-            cursor.execute(sql)
-            results = cursor.fetchall()
-            for row in results:
-                i7[row[0]] = {'all1': row[1], 'all2': row[2], 'all3': row[3], 'all4': row[4],
-                              'allcount': row[5], 'act1': row[6], 'act2': row[2], 'act3': row[3],
-                              'act4': row[4],
-                              'actcount': row[5]}
-        except:
-            print '\033[1;31;m'
-            print Path.tb_i_feature[i]
-            print '\033[0m'
-            db.rollback()
+        for item_id, all1, all2, all3, all4, allcount, act1, act2, act3, act4, actcount in reader:
+            i7[item_id] = {'all1': all1, 'all2': all2, 'all3': all3, 'all4': all4,
+                           'allcount': allcount, 'act1': act1, 'act2': act2, 'act3': act3,
+                           'act4': act4,
+                           'actcount': actcount}
         for item in i7:
             i7[item] = dict(i7[item], **i_all[item])
-        cursor = db.cursor()
-
-        sql = "select DISTINCT item_id from " + Path.tb_train_result[i] + " where behavior_type='4'"
-        i7_result = {}
-        try:
-            cursor.execute(sql)
-            results = cursor.fetchall()
-            for row in results:
-                i7_result[row[0]] = 1
-        except:
-            print '\033[1;31;m'
-            print 'db.rollback()7result'
-            print '\033[0m'
-            db.rollback()
-
+        i7_result = get_result(i)
         for item in i7_result:
             if i7.has_key(item):
                 i7[item]['result'] = 1
@@ -79,7 +79,6 @@ def i_train():
                  int(i7[item]['all_act4']),
                  int(i7[item]['all_actcount']),
                  int(i7[item]['result'])])
-    db.close()
     return array(i_list)
 
 
@@ -196,6 +195,7 @@ from sklearn.linear_model import LogisticRegression
 
 if __name__ == '__main__':
     data = i_train()
+    print shape(data)
     # train(data)
     # f = LogisticRegression(class_weight={1: 6.38775510204}, C=11.2857142857)
     # class_weight={1:2}
